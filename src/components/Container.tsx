bffc2926b621802f886c46671f1d43f7a559b8ae
Aron Homberg <info@aron-homberg.de>
2022-05-08 16:21:26 +0200

    fix: Container applies className now

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -12,8 +12,8 @@ const alignTitleCenterStyle = { marginLeft: 'auto', marginRight: 'auto'}
 
 const alignTitleRightStyle = { marginLeft: 'auto', marginRight: 0 }
 
-export const Container = ({ id, title, children, style, roundedCorners: roundedCornders, align, alignTitle }: ContainerProps) => (
-  <section id={id} className={`nes-ui-container ${title ? 'nes-ui-with-title' : ''} ${roundedCornders ? 'nes-ui-is-rounded' : ''}  ${align ? 'nes-ui-is-' + align : ''}`} style={style}>
+export const Container = ({ id, title, children, style, roundedCorners: roundedCornders, align, alignTitle, className }: ContainerProps) => (
+  <section id={id} className={`nes-ui-container ${title ? 'nes-ui-with-title' : ''} ${roundedCornders ? 'nes-ui-is-rounded' : ''}  ${align ? 'nes-ui-is-' + align : ''} ${className ? className : ''}`} style={style}>
     {title && (<h3 className={`nes-ui-title`} style={{ ...style, ...alignTitle === 'center' ? alignTitleCenterStyle : alignTitle === 'right' ? alignTitleRightStyle : {}}}>{title}</h3>)}
     {children}
   </section>
